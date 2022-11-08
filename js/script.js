e4cf71a0afdd8f95ed90ffe646d62c68bb47f984
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-11-08 09:02:41 -0500

    fin de portfolio con js para las barras sección SKILLS

--- js/script.js
+++ js/script.js
@@ -24,3 +24,18 @@ function responsiveMenu() {
         x.className = "";
     }
 }
+
+//detecto el scrolling para aplicar la animación de la barra de habilidades
+window.onscroll = function(){ efectoHabilidades()};
+
+//Función que aplica la animación de la barra de habilidades
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300) {
+        document.getElementById("html").classList.add("barra-progreso1");
+        document.getElementById("js").classList.add("barra-progreso2");
+        document.getElementById("bd").classList.add("barra-progreso3");
+        document.getElementById("php").classList.add("barra-progreso4");
+    }
+}
