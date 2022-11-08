2b187abf35a28b9caa68cb19c49f0969f7d3f854
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-11-08 07:29:05 -0500

    aplicando JS para RESPONSIVE del ícono MENÚ

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
+// función que me aplica el estilo a la opción seleccionada en el menú y quita la previamente seleccionada
+
+function seleccionar(link) {
+    var opciones = document.querySelector('#links a');
+    opciones[0].className = "";
+    opciones[1].className = "";
+    opciones[2].className = "";
+    opciones[3].className = "";
+    opciones[4].className = "";
+
+    //Hacemos desaparecer el menú una vez que se ha seleccionado una opción en el modo responsive
+    var x = document.getElementById("nav");
+    x.className = "";
+}
+
 // función que muestra el menú responsive
 
 function responsiveMenu() {
