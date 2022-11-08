4ad2b160a6904745a8cabba0bb7e1cf94905ea1a
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2022-11-08 07:34:47 -0500

    aplicando css y js para clase SELECCIONADO

--- js/script.js
+++ js/script.js
@@ -1,12 +1,13 @@
 // función que me aplica el estilo a la opción seleccionada en el menú y quita la previamente seleccionada
 
 function seleccionar(link) {
-    var opciones = document.querySelector('#links a');
+    var opciones = document.querySelectorAll('#links a');
     opciones[0].className = "";
     opciones[1].className = "";
     opciones[2].className = "";
     opciones[3].className = "";
     opciones[4].className = "";
+    link.className = "seleccionado";
 
     //Hacemos desaparecer el menú una vez que se ha seleccionado una opción en el modo responsive
     var x = document.getElementById("nav");
